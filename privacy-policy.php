7a105f8eb3194240810e6f35cf4cf0756629e31f
WppWpPanda <panda@wp-panda.pro>
2025-04-03 16:58:38 +0300

    privacy-policy page

---
+++ privacy-policy.php
@@ -0,0 +1,84 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once 'templates/header.php';
+defined('ABS_PATH' or exit('No direct script access allowed')); ?>
+    <section id="reviews" class="">
+        <div class="content-wrap">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="header-short">
+                            <h2 class="header-short__text">Privacy Policy</h2>
+                            <div class="header-short__line"></div>
+                            <div class="header-short__line"></div>
+                            <a href="reservation<?php echo PHP ?>" class="btn-yellow header-short__btn">Reservation</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="wrap bg-light">
+            <div class="content-wrap">
+                <div class="content-wrap content-wrap--top">
+                    <div class="container-fluid wpp-content">
+                        <div class="row">
+
+                            <ol class="wp-block-list">
+                                <li><strong>Introduction</strong><br>Welcome to Seattle Elite Towncar. We are committed
+                                    to respecting the privacy of all visitors and users. This Privacy Policy outlines
+                                    our practices regarding the collection, use, and disclosure of your information
+                                    through the use of our website.
+                                </li>
+
+
+                                <li><strong>Information Collection</strong><br>Personal Information: We collect personal
+                                    information you provide to us, such as your name, email address, and phone number,
+                                    when you register, subscribe, or interact with our site.<br>Non-Personal
+                                    Information: We also collect non-personal information, such as browser type,
+                                    operating system, and IP address, through cookies and other tracking technologies.
+                                </li>
+
+
+                                <li><strong>Use of Information</strong><br>Your information is used to:<br>Provide,
+                                    maintain, and improve our services.<br>Communicate with you about our services,
+                                    offers, and updates.<br>Enhance site security and user experience.
+                                </li>
+
+
+                                <li><strong>Information Sharing</strong><br>We do not sell, trade, or rent your personal
+                                    information to others. We may share information with third-party service providers
+                                    to help us operate our business, such as for hosting services or customer support,
+                                    under confidentiality agreements.
+                                </li>
+
+
+                                <li><strong>Data Security</strong><br>We take reasonable measures to protect the
+                                    information we collect from loss, theft, misuse, and unauthorized access or
+                                    disclosure.
+                                </li>
+
+
+                                <li><strong>Children’s Privacy</strong><br>Our website is not intended for children
+                                    under the age of 13. We do not knowingly collect personal information from children
+                                    under 13.
+                                </li>
+
+
+                                <li><strong>Changes to This Policy</strong><br>We may update this Privacy Policy
+                                    periodically. We will notify you of any changes by posting the new Privacy Policy on
+                                    this page.
+                                </li>
+
+
+                                <li><strong>Contact Us</strong><br>If you have any questions about this Privacy Policy,
+                                    please contact us at INFO@SEATTLEELITETOWNCAR.COM
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/templates/footer.php';
